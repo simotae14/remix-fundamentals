1cbdb605702ab5fd8f891bdb2753fb88c8da074b
Simone Taeggi <startae14@gmail.com>
2024-10-02 00:25:06 +0200

    02 Data Loading exercise

--- exercise/02-data-loading/app/routes/posts/index.tsx
+++ exercise/02-data-loading/app/routes/posts/index.tsx
@@ -1,7 +1,49 @@
+import { Link, useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/node";
+
+type Post = {
+  slug: string;
+  title: string;
+};
+
+async function getPosts() {
+  return [
+    {
+      slug: "my-first-post",
+      title: "My First Post",
+    },
+    {
+      slug: "90s-mixtape",
+      title: "A Mixtape I Made Just For You",
+    },
+  ];
+}
+
+export const loader = async () => {
+  const posts = await getPosts();
+  return json({posts}); // <-- return the data from the loader
+  // return new Response(JSON.stringify(posts), {
+  //   status: 200,
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  // });
+};
+
 export default function Posts() {
+  const {posts} = useLoaderData<typeof loader>(); // <-- get the data into your UI
   return (
     <main>
       <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link to={post.slug} className="text-blue-600 underline">
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
